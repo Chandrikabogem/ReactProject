94972ce397c2a960ca967ef52f82315758d14249
saivenkat22 <saivenkat22>
2022-08-05 12:17:36 +0530

    Commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import HttpGet from './components/HttpGet';
+import HttpPost from './components/HttpPost';
 
 function App() {
   return (
     <div className="App">
-      <HttpGet/>
+      <HttpPost/>
     </div>
   );
 }
