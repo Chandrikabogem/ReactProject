cc57c0c5ff52e35f447cc3145f9a05213e12c5b9
saivenkat22 <saivenkat22>
2022-08-05 11:36:36 +0530

    commit

---
+++ src/components/HttpPost.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+
+function App() {
+
+  const[data,setData]:any=useState()
+  const [inputData,setInputData]:any=useState()
+
+  function dataE(event:any):any{
+    console.log(event.target.value);
+    // setInputData(name:)
+
+    
+  }
+
+  function get():any{
+    axios.get('https://jsonplaceholder.typicode.com/users')
+    .then((res)=>{
+      console.log(res);
+      setData(res.data)
+    })
+  }
+
+  function post():any{
+    axios.get('https://jsonplaceholder.typicode.com/users')
+    .then((res)=>{
+      console.log(res.data);
+      setData(res.data)
+    })
+  }
+
+  return (
+    <div className="App">
+      <button onClick={get}>Get</button>
+      <br />
+      <input placeholder='Enter Name' name='name' onChange={dataE}></input>
+      <input placeholder='Enter Email' name='email'></input>
+      <input placeholder='Enter Mobile Number' name='mobileNumber'></input>
+      <button onClick={post}>Post</button>
+    </div>
+  );
+}
+
+export default App;
